64f927712320bca0f13bae371bf66056f29244dd
Ashutosh Padhy <ashutoshpadhy001@gmail.com>
2024-01-18 00:06:49 +0530

    fixes

--- client/src/components/LeaderBoard.jsx
+++ client/src/components/LeaderBoard.jsx
@@ -43,24 +43,36 @@ const Leaderboard = () => {
       ) : (
         <div className="max-w-2xl mx-auto mt-8 p-4 bg-white shadow-md rounded-md">
           <h2 className="text-2xl font-bold mb-4">Leaderboard</h2>
-          <table className="min-w-full border border-gray-300">
-            <thead>
-              <tr className="bg-gray-200">
-                <th className="py-2 px-4 border">Rank</th>
-                <th className="py-2 px-4 border">Username</th>
-                <th className="py-2 px-4 border">Total Score</th>
-              </tr>
-            </thead>
-            <tbody>
-              {sortedUsers.map((user, index) => (
-                <tr key={user._id}>
-                  <td className="py-2 px-4 border">{index + 1}</td>
-                  <td className="py-2 px-4 border">{user.username}</td>
-                  <td className="py-2 px-4 border">{user.totalScore}</td>
+          <p className="mb-3">
+            The points are calculated by taking a sum of all the points scored
+            by user across all the exercises they have attended in a particular
+            language.
+          </p>
+          {sortedUsers.length > 0 ? (
+            <table className="min-w-full border border-gray-300">
+              <thead>
+                <tr className="bg-gray-200">
+                  <th className="py-2 px-4 border">Rank</th>
+                  <th className="py-2 px-4 border">Username</th>
+                  <th className="py-2 px-4 border">Total Score</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {sortedUsers.map((user, index) => (
+                  <tr key={user._id}>
+                    <td className="py-2 px-4 border">{index + 1}</td>
+                    <td className="py-2 px-4 border">{user.username}</td>
+                    <td className="py-2 px-4 border">{user.totalScore}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p className="text-red-500">
+              No Leaderboard found, perhaps no user attended any exercise on
+              this language.
+            </p>
+          )}
         </div>
       )}
     </>
--- client/src/pages/QuizExercisePage.jsx
+++ client/src/pages/QuizExercisePage.jsx
@@ -29,24 +29,28 @@ const Exercises = () => {
           <p>Loading Exercises</p>
         ) : (
           <>
-            {exercises.map((exercise) => (
-              <li key={exercise._id} className="border p-4 rounded-md">
-                <div className="flex justify-between items-center">
-                  <div className="flex flex-col">
-                    <h3 className="text-xl font-bold">{exercise.title}</h3>
-                    <p className="text-sm mb-2">
-                      Difficulty: {exercise.difficulty}
-                    </p>
+            {exercises.length > 0 ? (
+              exercises.map((exercise) => (
+                <li key={exercise._id} className="border p-4 rounded-md">
+                  <div className="flex justify-between items-center">
+                    <div className="flex flex-col">
+                      <h3 className="text-xl font-bold">{exercise.title}</h3>
+                      <p className="text-sm mb-2">
+                        Difficulty: {exercise.difficulty}
+                      </p>
+                    </div>
+                    <Link
+                      to={`/${languageId}/exercise/${exercise._id}`}
+                      className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 text-center"
+                    >
+                      Start Exercise
+                    </Link>
                   </div>
-                  <Link
-                    to={`/${languageId}/exercise/${exercise._id}`}
-                    className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 text-center"
-                  >
-                    Start Exercise
-                  </Link>
-                </div>
-              </li>
-            ))}
+                </li>
+              ))
+            ) : (
+              <p>No exercises added</p>
+            )}
           </>
         )}
       </ul>
